cdf2b126b9c92b9cdc112609b9d6f974931a1d65
Ignacio Paz <natxio.dev@gmail.com>
2023-11-28 10:49:04 -0400

    add: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
 	dotenv.config();
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	app.enableCors({
+		origin: '*'
+	});
+	await app.listen(5000);
 }
 bootstrap();
